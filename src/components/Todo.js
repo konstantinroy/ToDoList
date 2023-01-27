315b9e5e29b6c96433d2cda91ea42ff58d53bbc4
Константин Рой <konstantin.roy9977@gmail.com>
2023-01-27 08:16:35 +0300

    221

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,6 +4,7 @@ import { RiDeleteBin2Line } from "react-icons/ri";
 import { BsCheckLg } from "react-icons/bs";
 import { VscChromeClose } from "react-icons/vsc";
 import styles from "./Todo.module.scss";
+////
 
 function Todo({ todo, deleteTodo, toggleTodo }) {
   const divRef = useRef(null);
